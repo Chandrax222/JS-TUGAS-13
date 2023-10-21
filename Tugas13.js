99d5caf8583a0cc0f244057d8c5f5a7e587c83a2
Chandra Wiratama Putera <141868663+Chandrax222@users.noreply.github.com>
2023-10-21 22:59:50 +0700

    inshif pada buahan
    inshif pada buahan

---
+++ Tugas13.js
@@ -0,0 +1,10 @@
+function Tugas13() {
+    var buahan = ["Pisang", "Jeruk"];
+
+    console.log(buahan);
+    var buahan2 = buahan.unshift("Apel", "Mangga");
+    console.log(buahan2);
+    return buahan;
+}
+console.log(Tugas13());
+
